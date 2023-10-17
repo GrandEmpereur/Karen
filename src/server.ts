c0bce665ead05fa3ae5d2ccf2bb5da07c39edcc9
Empereur <bartosikpatrick2@gmail.com>
2023-10-17 10:46:31 +0100

    🎉 Init(Projet) : Configuration initiale du projet avec Koa et Docker

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import Koa from "koa";
+import cors from "@koa/cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import home from "./routes/main";
+
+dotenv.config({
+    path: path.resolve(process.cwd(), process.env.NODE_ENV === "development" ? '.env.development' : '.env'),
+});
+
+async function main() {
+    const app = await createApp();
+    const port = process.env.PORT || 3100;
+    console.log(`Server listening on port http://localhost:${port}`);
+    app.listen(port);
+}
+
+/**
+ * @description The core of Koa, launch the Koa server
+ * @returns a koa app
+ */
+async function createApp(): Promise<Koa> {
+    const app = new Koa() as Koa;
+    app.use(cors());
+    app.use(home.routes());
+    return app;
+}
+
+main();
---
+++ src/services/auth.ts
@@ -0,0 +1,18 @@
+import { RateLimit } from "koa2-ratelimit";
+
+const RATE_LIMIT = 5;
+const REQUEST_CALL_BY_MINUTES = 1
+
+/**
+ * Limits the number of requests to a given number in a given time frame
+ * @param {number} max - number - The number of requests allowed in the time frame.
+ * @param {number} timeFrameInMinutes - The time frame in minutes in which the limit is applied.
+ * @returns A function that takes in two parameters and returns a function.
+ */
+export const rateLimiter = (max: number, timeFrameInMinutes: number) => {
+    return RateLimit.middleware({
+        interval: { min: timeFrameInMinutes },
+        max: max,
+        message: `Request limit exceeded`,
+    });
+};

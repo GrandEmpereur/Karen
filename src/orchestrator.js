1effd04fd404d1757aecf52e862c15b216b3f0d2
Empereur <bartosikpatrick2@gmail.com>
2023-10-18 22:35:34 +0100

    🔄 refactor(package.json) : supprimer la clé "main" du fichier package.json La clé "main" n'est plus nécessaire car le fichier server.js n'est plus utilisé comme point d'entrée principal de l'application. Cette modification simplifie le fichier package.json en supprimant une clé inutile.
    🔄 refactor(orchestrator.js) : réorganiser le code pour améliorer la lisibilité
    Le code a été réorganisé pour améliorer la lisibilité et la compréhension. Les commentaires ont été ajoutés pour expliquer chaque étape de l'orchestration. Aucun changement fonctionnel n'a été apporté.
    
    🗑️ supprimer(generateConfig.ts) : supprimer le fichier generateConfig.ts
    Le fichier generateConfig.ts a été supprimé car il n'est plus utilisé dans l'application. Cette suppression permet de nettoyer le code et de supprimer du code inutile.
    
    🔄 refactor(sendData.test.js) : supprimer les imports inutiles et les dépendances non utilisées
    Les imports inutiles et les dépendances non utilisées ont été supprimés du fichier sendData.test.js. Cela permet de nettoyer le code et d'améliorer la lisibilité.
    
    🔄 refactor(userInput.test.js) : supprimer les imports inutiles et les dépendances non utilisées
    Les imports inutiles et les dépendances non utilisées ont été supprimés du fichier userInput.test.js. Cela permet de nettoyer le code et d'améliorer la lisibilité.

--- src/orchestrator.js
+++ src/orchestrator.js
@@ -4,22 +4,22 @@ const path = require('path');
 const { sendConfig } = require('./sendData');
 
 async function orchestrate() {
-    try {
-        // Lancer le script userInput
-        execSync('node userInput.js', { stdio: 'inherit' });
+  try {
+    // Lancer le script userInput
+    execSync('node userInput.js', { stdio: 'inherit' });
 
-        // Lancer la fonction sendConfig de sendData
-        await sendConfig();
+    // Lancer la fonction sendConfig de sendData
+    await sendConfig();
 
-        // Supprimer la ligne postinstall du package.json
-        const packagePath = path.resolve(__dirname, 'package.json');
-        const packageJson = require(packagePath);
-        delete packageJson.scripts.postinstall;
-        fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
-        console.log('Ligne postinstall supprimée du package.json');
-    } catch (error) {
-        console.error('Erreur lors de l\'orchestration :', error);
-    }
+    // Supprimer la ligne postinstall du package.json
+    const packagePath = path.resolve(__dirname, 'package.json');
+    const packageJson = require(packagePath);
+    delete packageJson.scripts.postinstall;
+    fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
+    console.log('Ligne postinstall supprimée du package.json');
+  } catch (error) {
+    console.error('Erreur lors de l\'orchestration :', error);
+  }
 }
 
 orchestrate();

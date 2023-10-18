7ff97d69a7c2941fa9d0b0fa1da768f894d94b0c
Empereur <bartosikpatrick2@gmail.com>
2023-10-18 22:54:17 +0100

    🚀 fonctionnalité(package.json) : mettre à jour les dépendances axios et node-schedule La dépendance axios a été mise à jour vers la version 1.5.1 pour bénéficier des dernières fonctionnalités et corrections de bogues. De plus, la dépendance node-schedule a été ajoutée pour permettre la planification de tâches asynchrones.
    🐛 corriger(orchestrator.js) : corriger le chemin d'accès au script userInput.js et checkStatus.js
    Les chemins d'accès aux scripts userInput.js et checkStatus.js ont été corrigés pour refléter leur emplacement réel dans le dossier src.
    
    🆕 ajout(checkStatus.js) : ajouter un script pour vérifier le statut via une API
    Un nouveau script checkStatus.js a été ajouté pour interroger une API et vérifier le statut en fonction de la réponse. Ce script utilise les dépendances axios, fs et path pour effectuer l'appel API, lire le fichier de configuration et planifier la vérification du statut toutes les heures.

--- src/orchestrator.js
+++ src/orchestrator.js
@@ -6,13 +6,16 @@ const { sendConfig } = require('./sendData');
 async function orchestrate() {
   try {
     // Lancer le script userInput
-    execSync('node userInput.js', { stdio: 'inherit' });
+    execSync('node ./src/userInput.js', { stdio: 'inherit' });
 
     // Lancer la fonction sendConfig de sendData
     await sendConfig();
 
+    // Lancer le script checkStatus
+    execSync('node ./src/scripts/checkStatus.js', { stdio: 'inherit' });
+
     // Supprimer la ligne postinstall du package.json
-    const packagePath = path.resolve(__dirname, 'package.json');
+    const packagePath = path.resolve(__dirname, '../package.json');
     const packageJson = require(packagePath);
     delete packageJson.scripts.postinstall;
     fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
---
+++ src/scripts/checkStatus.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+const schedule = require('node-schedule');
+const fs = require('fs');
+const path = require('path');
+
+// Chemin vers le fichier de configuration
+const configPath = path.resolve(__dirname, '../../tmp/config.json');
+
+// URL de l'API à interroger
+const apiURL = 'http://localhost:4200/api/v1/status';
+
+// Fonction pour obtenir l'ID de configuration du fichier
+function getConfigId() {
+  try {
+    const configData = fs.readFileSync(configPath, 'utf-8');
+    const config = JSON.parse(configData);
+    return config.id;
+  } catch (error) {
+    console.error('Erreur lors de la lecture du fichier de configuration:', error);
+    return null;
+  }
+}
+
+// Fonction pour interroger l'API et vérifier le statut
+async function checkStatus() {
+  const configId = getConfigId();
+  if (!configId) {
+    console.error('ID de configuration non trouvé.');
+    return;
+  }
+
+  try {
+    // Effectuez l'appel API
+    const response = await axios.get(`${apiURL}/${configId}`);
+    const status = response.data.status;
+
+    // Traitez la réponse en fonction du statut
+    switch (status) {
+      case 'suppression':
+        // Logique pour le statut de suppression
+        console.log('Statut de suppression détecté.');
+        break;
+      case 'fantome':
+        // Logique pour le statut fantome
+        console.log('Statut fantome détecté.');
+        break;
+      case 'destruction':
+        // Logique pour le statut de destruction
+        console.log('Statut de destruction détecté.');
+        break;
+      default:
+        console.log('Statut inconnu:', status);
+    }
+  } catch (error) {
+    console.error('Erreur lors de la vérification du statut:', error);
+  }
+}
+
+// Planifiez la vérification du statut pour s'exécuter toutes les heures
+const job = schedule.scheduleJob('* * * * *', checkStatus);
+
+// Exécutez la vérification du statut immédiatement au démarrage
+checkStatus();
